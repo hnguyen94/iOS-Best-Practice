1678926bb295cd7f15d62ec301930a37d438f880
Hoang Duc Nguyen <hd.ngy@Hoangs-MacBook-Pro.local>
2019-04-09 10:44:32 +0100

    Back to original vc

--- Coordinators/Coordinators/BuyViewController.swift
+++ Coordinators/Coordinators/BuyViewController.swift
@@ -10,12 +10,18 @@ import UIKit
 
 class BuyViewController: UIViewController, Storyboarded {
 
-    weak var coordinator: MainCoordinator?
+    weak var coordinator: BuyCoordinator?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        coordinator?.didFinishBuying()
     }
 
     deinit {
